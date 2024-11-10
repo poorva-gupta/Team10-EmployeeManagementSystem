2d60cb4727f642692f1d43f3adb2a095cb838440
poorva-gupta <guptapoorva1203@gmail.com>
2024-11-10 22:17:35 +0530

    Update ConnectionManager.java as maven project

--- src/main/java/com/example/ConnectionManager.java
+++ src/main/java/com/example/ConnectionManager.java
@@ -1,41 +1,5 @@
-// // import java.sql.*;
-// // import java.sql.Connection;
-// // import java.sql.Statement;
+package com.example.employeemanagement;
 
-// // public class connection {
-// //     Connection connection;
-// //     Statement statement;
-
-// //     public connection(){
-// //         try{
-// //             Class.forName("cpm.mysql.cj.jdbc.Driver");
-// //             connection=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employeemanagement","root","ZXdf@#24");
-// //             statement=connection.createStatement();
-
-// //         }catch(Exception e){
-// //             e.printStackTrace();
-// //         }
-// //     }
-    
-
-    
-// // }
-// import java.sql.*;
-
-// public class ConnectionManager {
-//     Connection connection;
-//     Statement statement;
-
-//     public ConnectionManager() {
-//         try {
-//             Class.forName("com.mysql.cj.jdbc.Driver");
-//             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/employeemanagement", "root", "ZXdf@#24");
-//             statement = connection.createStatement();
-//         } catch (Exception e) {
-//             e.printStackTrace();
-//         }
-//     }
-// }
 import java.sql.*;
 
 public class ConnectionManager {
